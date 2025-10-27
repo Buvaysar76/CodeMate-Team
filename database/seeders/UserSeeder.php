a6cebfecda7280c1c30275727fe500df971daf50
Buvaysar <buvaysar2032@gmail.com>
2025-10-27 21:11:26 +0300

    feat(seed): добавлены сидеры для тестовых пользователей

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'User One',
+            'email' => 'user1@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::create([
+            'name' => 'User Two',
+            'email' => 'user2@example.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
